e2ae29d2a11cbad061a81ec28759ed5fbcce99f0
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-10-17 16:57:51 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const number = 21;
+
+let nome = prompt('Quale è il tuo nome');
+console.log(nome);
+let cognome = prompt('Quale è il tuo cognome');
+console.log(cognome);
+let colorePreferito = prompt('Qual è il tuo colore preferito');
+console.log(colorePreferito)
+let password = nome + cognome + colorePreferito + number;
+console.log (password, typeof password );
+document.getElementById("password").innerHTML = password;
